26c30e30633c303647f3a6b87eade96cd16824fa
YskaArt <franciscomorello99@gmail.com>
2024-12-09 12:50:09 -0300

    Last Update
    Añadí sistema de particulas para mejorar el feedback al jugador sobre el impacto de los cuchillos a los enemigos

--- Assets/Scripts/Mecanicas/Bullet.cs
+++ Assets/Scripts/Mecanicas/Bullet.cs
@@ -5,12 +5,24 @@ using UnityEngine;
 public class Bullet : MonoBehaviour
 {
     public float TempoForDestroy = 10f; // Tiempo en segundos antes de eliminar el objeto
+    public GameObject bloodParticlesPrefab;
 
-   
     void Start()
     {
         // Llama a la función Eliminar tras el intervalo de tiempo especificado
         Destroy(gameObject, TempoForDestroy);
     }
-   
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Enemy")) 
+        {
+            // Instancia las partículas de sangre en el punto de colisión.
+            Instantiate(bloodParticlesPrefab, collision.contacts[0].point, Quaternion.identity);
+
+           
+        }
+    }
+
+
 }
